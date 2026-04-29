82fe50663272c935b37cbc696b8b56cbac96ea6b
ahsanwebdeveloper <ahsanaliecomrice@gmail.com>
2026-04-28 23:07:52 -0700

    add robot.txt file

---
+++ app/robots.js
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://ahsanali-dev.vercel.app/sitemap.xml',
+  }
+}
